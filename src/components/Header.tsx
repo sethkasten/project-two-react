0ca4e826102fe19d5d8bd3b5bf70d80c98d282e1
jeff robinson <jeff.h.robinson@icloud.com>
2021-11-08 09:51:22 -0500

    main branch commit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,22 @@
+import { Link } from "react-router-dom";
 import "./Header.css";
 
 const Header = () => {
-  return <div className="Header">Header works</div>;
+  return (
+    <div className="Header">
+      <Link to="/">
+        <h1>Event Finder 9001</h1>
+      </Link>
+      <Link to="/events/bucketlist">
+        <h2>Bucket List</h2>
+      </Link>
+      <h2>
+        <a href="https://developer.ticketmaster.com/">
+          Powered by TicketMaster
+        </a>
+      </h2>
+    </div>
+  );
 };
 
 export default Header;
