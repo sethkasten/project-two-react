34a8448f71c2f1ca8e1a440d6d298ed4aa0b34e9
jeff robinson <jeff.h.robinson@icloud.com>
2021-11-08 09:40:09 -0500

    commit this

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,41 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Details from "./components/Details";
+import BucketList from "./components/BucketList";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/gifs/search" exact>
+           <Home />
+          </Route>
+          <Route path="/gifs/:id/details" exact>
+            <Details />
+          </Route>
+          <Route path="/gifs/favorites">
+            <BucketList />
+          </Route>
+          <Route path="/">
+            <Redirect to="/" />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
